059d947710ee125564d44c2dad8008c72e8d4293
Belen <belenn2206@gmail.com>
2022-09-18 23:37:42 -0300

    terminado

---
+++ Ejercicio C02_Enciendan sus motores/AutoF1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_C02_Enciendan_sus_motores
+{
+    public class AutoF1
+    {
+        private short cantidadCombustible;//
+        private bool enCompetencia;//
+        private string escuderia;
+        private short numero;
+        private short vueltasRestantes;//
+
+        public AutoF1(string escuderia, short numero)
+        {
+            this.cantidadCombustible = 0;
+            this.enCompetencia =false;
+            this.escuderia = escuderia;
+            this.numero = numero;
+            this.vueltasRestantes = 0;
+        }
+        #region GETTERS & SETTERS
+        public short GetCantidadCombustible()
+        {
+            return this.cantidadCombustible;
+        }
+        public bool EnCompetencia()
+        {
+            return this.enCompetencia;
+        }
+        public short  GetVueltasRestantes()
+        {
+            return this.vueltasRestantes;
+        }
+        public void SetCantidadCombustible(short cantidad)
+        {
+            this.cantidadCombustible = cantidad;
+        }
+        public void SetEnCompetencia(bool enCompetencia)
+        {
+            this.enCompetencia = enCompetencia;
+        }
+        public void SetVueltasRestantes(short vueltas)
+        {
+            this.vueltasRestantes = vueltas;
+        }
+        #endregion
+
+        public string MostrarDatos()
+        {
+            string sigue = "no";
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Cantidad de Combustible: {cantidadCombustible}");
+            if(enCompetencia)
+            {
+                sigue = "si";
+            }
+            sb.AppendLine($"En competencia: {sigue}");
+            sb.AppendLine($"Escuderia: {escuderia}");
+            sb.AppendLine($"Numero: {numero}");
+            sb.AppendLine($"Vueltas restantes: {vueltasRestantes}");
+            return sb.ToString();
+        }
+        public static bool operator ==(AutoF1 auto1, AutoF1 auto2)
+        {
+            if(auto1.numero == auto2.numero && auto1.escuderia == auto2.escuderia)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator !=(AutoF1 auto1, AutoF1 auto2)
+        {
+            return !(auto1 == auto2);
+        }
+    }
+}
---
+++ Ejercicio C02_Enciendan sus motores/Competencia.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+
+namespace Ejercicio_C02_Enciendan_sus_motores
+{
+    public class Competencia
+    {
+        private short cantidadCompetidores;
+        private short cantidadVuletas;
+        private List<AutoF1> competidores;
+
+        private Competencia()
+        {
+            cantidadCompetidores = 0;
+            cantidadVuletas = 0;
+            competidores = new List<AutoF1>();
+        }
+        public Competencia(short cantidadCompetidores, short cantidadVuletas): this()
+        {
+            this.cantidadCompetidores = cantidadCompetidores;
+            this.cantidadVuletas = cantidadVuletas;
+        }
+        public string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Cantidad de Competidores: {cantidadCompetidores}");
+            sb.AppendLine($"Cantidad de vueltas: {cantidadVuletas}");
+            foreach(AutoF1 auto in competidores)
+            {
+                sb.AppendLine($"Competidores: {auto.MostrarDatos()}");
+            }
+            return sb.ToString();
+        }
+        public static bool operator ==(Competencia c, AutoF1 a)
+        {
+            foreach(AutoF1 auto in c.competidores)
+            {
+                if(auto == a)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool operator !=(Competencia c, AutoF1 a)
+        {
+            return !(c == a);
+        }
+        public static bool operator +(Competencia c, AutoF1 a)
+        {
+            Random rand = new Random();
+          
+            if(c.competidores.Count < c.cantidadCompetidores && c!= a)
+            {
+                c.competidores.Add(a);
+                a.SetEnCompetencia(true);
+                a.SetVueltasRestantes(c.cantidadVuletas);
+                a.SetCantidadCombustible((short)rand.Next(15, 100));
+                return true;
+            }
+            return false;
+        }
+        public static bool operator -(Competencia c, AutoF1 a)
+        {
+            if(c==a)
+            {
+                c.competidores.Remove(a);
+                return true;
+            }
+            return false;
+        }
+    }
+}
